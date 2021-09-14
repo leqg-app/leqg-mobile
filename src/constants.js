b57ee6313932444282e3be5719017a63c0089d57
Nicolas de Chevigné <nico2che@gmail.com>
2021-09-14 19:08:32 +0200

    Use constants file

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const daysShort = ['L', 'M', 'M', 'J', 'V', 'S', 'D'];
+export const daysFull = [
+  'lundi',
+  'mardi',
+  'mercredi',
+  'jeudi',
+  'vendredi',
+  'samedi',
+  'dimanche',
+];
