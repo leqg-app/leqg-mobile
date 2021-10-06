3c578be1ad45d9eae0920e5bfa49f08fbf00808e
Nicolas de Chevigné <nico2che@gmail.com>
2021-10-06 23:21:19 +0200

    Improve select product

--- src/SelectProduct.js
+++ src/SelectProduct.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
-import { SafeAreaView, SectionList, StyleSheet } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
 import {
   ActivityIndicator,
   Appbar,
-  Text,
   Searchbar,
   DataTable,
 } from 'react-native-paper';
 
+import Header from './components/Header';
 import { useStore } from './store/context';
 
 const SelectProduct = ({ navigation }) => {
@@ -20,36 +20,24 @@ const SelectProduct = ({ navigation }) => {
     }
   }, []);
 
-  const data = products.reduce((products, product) => {
-    const group = product.charAt(0);
-    if (!products[group]) {
-      products[group] = { title: group, data: [] };
-    }
-    products[group].data.push(product);
-    return products;
-  }, {});
-
   return (
     <SafeAreaView style={styles.container}>
-      <Appbar.Header>
+      <Header>
         <Appbar.BackAction onPress={() => navigation.goBack()} />
         <Appbar.Content title="Choisir une bière" />
-      </Appbar.Header>
+      </Header>
       <Searchbar placeholder="Rechercher une bière" style={styles.searchBar} />
       {loading ? (
         <ActivityIndicator animating={true} />
       ) : (
-        <SectionList
-          sections={Object.values(data)}
-          renderItem={({ item }) => (
+        <FlatList
+          data={products}
+          renderItem={product => (
             <DataTable.Row>
-              <DataTable.Cell numeric>{item}</DataTable.Cell>
+              <DataTable.Cell>{product.name}</DataTable.Cell>
             </DataTable.Row>
           )}
-          renderSectionHeader={({ section }) => (
-            <Text style={styles.sectionHeader}>{section.title}</Text>
-          )}
-          keyExtractor={(item, index) => index}
+          keyExtractor={product => product.id}
         />
       )}
     </SafeAreaView>
