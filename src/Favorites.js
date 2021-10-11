7b21fa677f320fdd3ff0c2c46da2a0441783501a
Nicolas de Chevigné <nico2che@gmail.com>
2021-10-11 23:42:34 +0200

    Display favorites list

--- src/Favorites.js
+++ src/Favorites.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { FlatList, SafeAreaView, StyleSheet, View } from 'react-native';
-import { Appbar, Button, DataTable, Paragraph } from 'react-native-paper';
+import {
+  Appbar,
+  Button,
+  Card,
+  IconButton,
+  Paragraph,
+} from 'react-native-paper';
 
 import Header from './components/Header';
 import { useStore } from './store/context';
@@ -12,37 +18,58 @@ const AuthRequired = ({ navigation }) => (
   </>
 );
 
+const Row =
+  actions =>
+  ({ item }) =>
+    (
+      <Card style={styles.row}>
+        <View style={styles.flex}>
+          <View style={styles.info}>
+            <Card.Title title={item.name} />
+            <Card.Content>
+              <Paragraph>{item.address}</Paragraph>
+            </Card.Content>
+          </View>
+          <View style={styles.star}>
+            <IconButton
+              icon="star"
+              color="green"
+              onPress={() => actions.removeFavorite(item)}
+            />
+          </View>
+        </View>
+      </Card>
+    );
+
 const Favorites = ({ navigation }) => {
-  const [{ user }] = useStore();
+  const [{ user }, actions] = useStore();
   return (
     <SafeAreaView style={styles.container}>
       <Header>
         <Appbar.Content title="Enregistrés" />
       </Header>
-      <View style={styles.center}>
-        {user.jwt ? (
-          user.details.favorites.length ? (
+      {user.jwt ? (
+        user.favorites.length ? (
+          <View style={styles.container}>
             <FlatList
-              data={user.details.favorites}
-              renderItem={favorite => (
-                <DataTable.Row>
-                  <DataTable.Cell>{favorite.id}</DataTable.Cell>
-                </DataTable.Row>
-              )}
+              data={user.favorites}
+              renderItem={Row(actions)}
               keyExtractor={favorite => favorite.id}
             />
-          ) : (
-            <>
-              <Paragraph>Vous n'avez pas encore de bar favoris.</Paragraph>
-              <Paragraph>
-                Explorez la carte pour en ajouter de nouveaux !
-              </Paragraph>
-            </>
-          )
+          </View>
         ) : (
+          <View style={styles.center}>
+            <Paragraph>Vous n'avez pas encore de bar favoris.</Paragraph>
+            <Paragraph>
+              Explorez la carte pour en ajouter de nouveaux !
+            </Paragraph>
+          </View>
+        )
+      ) : (
+        <View style={styles.center}>
           <AuthRequired navigation={navigation} />
-        )}
-      </View>
+        </View>
+      )}
     </SafeAreaView>
   );
 };
@@ -53,6 +80,25 @@ const styles = StyleSheet.create({
   },
   center: {
     flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  row: {
+    flex: 1,
+    paddingBottom: 15,
+    marginVertical: 7,
+    marginHorizontal: 20,
+  },
+  flex: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  info: {
+    width: '80%',
+  },
+  star: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
