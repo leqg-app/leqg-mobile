1d9ba8cf4216d4e8de1db20526e5e31a4c764ce7
Nicolas de Chevigné <nico2che@gmail.com>
2021-10-21 19:14:10 +0200

    Add account details

--- src/containers/Account/Account.js
+++ src/containers/Account/Account.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Appbar } from 'react-native-paper';
+import { Appbar, Subheading, Title } from 'react-native-paper';
 
 import Auth from './Auth';
 import Header from '../../components/Header';
 import { useStore } from '../../store/context';
 
 const Account = () => {
+  const [state] = useStore();
   return (
     <SafeAreaView>
       <Header>
         <Appbar.Content title="Mon compte" />
       </Header>
-      <View style={styles.box} />
+      <View style={styles.box}>
+        <Title>{state.user.username}</Title>
+        <Subheading>0 contribution</Subheading>
+      </View>
     </SafeAreaView>
   );
 };
