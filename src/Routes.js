ee4df1bcb45a9a56a124f3f7ed932bcefe5212dc
Nicolas de Chevigné <nico2che@gmail.com>
2021-10-21 19:13:44 +0200

    Revamp routes

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Map from './containers/Map/Map';
 import Favorites from './Favorites';
@@ -9,77 +10,69 @@ import Account from './containers/Account/Account';
 import EditStore from './containers/EditStore/EditStore';
 
 const Tab = createBottomTabNavigator();
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="MapTab"
+      screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="MapTab"
+        component={Map}
+        options={{
+          title: 'Carte',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="map" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="EditStoreTab"
+        component={EditStore}
+        options={{
+          title: 'Contribuer',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="plus" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="FavoritesTab"
+        component={Favorites}
+        options={{
+          title: 'Enregistrés',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="bookmark-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="AccountTab"
+        component={Account}
+        options={{
+          title: 'Compte',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
+const Main = createNativeStackNavigator();
 const Routes = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="MapTab"
+      <Main.Navigator
+        initialRouteName="TabNavigator"
         screenOptions={{ headerShown: false }}>
-        <Tab.Screen
-          name="MapTab"
-          component={Map}
-          options={{
-            title: 'Carte',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="map" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="EditStoreTab"
-          component={EditStore}
-          options={{
-            title: 'Contribuer',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="plus" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="FavoritesTab"
-          component={Favorites}
-          options={{
-            title: 'Enregistrés',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="bookmark-outline"
-                color={color}
-                size={size}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="AccountTab"
-          component={Account}
-          options={{
-            title: 'Compte',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="account"
-                color={color}
-                size={size}
-              />
-            ),
-          }}
-        />
-        {/*
-        {state.jwt && (
-          <>
-            <Tab.Screen
-              name="Contributions"
-              component={AddStore}
-              options={{ title: 'Mes contributions' }}
-            />
-            <Tab.Screen
-              name="Account"
-              component={AddStore}
-              options={{ title: 'Mon compte' }}
-            />
-          </>
-        )} */}
-      </Tab.Navigator>
+        <Main.Screen name="TabNavigator" component={TabNavigator} />
+        <Main.Screen name="EditStoreScreen" component={EditStore} />
+      </Main.Navigator>
     </NavigationContainer>
   );
 };
