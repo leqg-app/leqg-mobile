eff9e7ed4b29eded0fd8fb6365d58e48095b38c8
Nicolas de Chevigné <nico2che@gmail.com>
2021-11-21 16:11:32 +0100

    Revamp Store & display schedules

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,16 +1,22 @@
 export function formatHour(hour) {
   const { hours, minutes } = hour;
-  return [hours, minutes].map(n => String(n).padStart(2, '0')).join(':');
+  return [hours, minutes].map(n => String(n).padStart(2, '0')).join('h');
+}
+
+function pad2(n) {
+  return String(n).padStart(2, '0');
+}
+
+function toHour(hour, minute) {
+  return `${pad2(hour)}h${minute ? pad2(minute) : ''}`;
 }
 
 export function secondToTime(seconds) {
-  return formatHour({
-    hours: Math.floor(seconds / 3600),
-    minutes: Math.floor((seconds % 3600) / 60),
-  });
+  return toHour(Math.floor(seconds / 3600), Math.floor((seconds % 3600) / 60));
 }
 
 export function inHours(start, end) {
+  const date = new Date();
   const now = date.getHours() * 3600 + date.getMinutes() * 60;
   if (start > end) {
     // 17h - 2h
