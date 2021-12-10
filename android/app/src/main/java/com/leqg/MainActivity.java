e48f280897d28997d168bc6088784517c39f55f6
Nicolas de Chevigné <nico2che@gmail.com>
2021-12-10 21:01:18 +0100

    Upgrade dependencies

--- android/app/src/main/java/com/leqg/MainActivity.java
+++ android/app/src/main/java/com/leqg/MainActivity.java
@@ -1,7 +1,7 @@
 package com.leqg;
 
-import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
 import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
@@ -16,8 +16,14 @@ public class MainActivity extends ReactActivity {
   }
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this);
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+
+      @Override
+      protected void loadApp(String appKey) {
+        RNBootSplash.init(MainActivity.this);
+        super.loadApp(appKey);
+      }
+    };
   }
 }
