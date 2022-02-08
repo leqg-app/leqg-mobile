53f2340687f99f06784f5f845c77bc30184783a8
Nicolas de Chevigné <nico2che@gmail.com>
2022-02-08 00:10:58 -0800

    Disable dark mode and fix alert style

--- android/app/src/main/java/com/leqg/MainApplication.java
+++ android/app/src/main/java/com/leqg/MainApplication.java
@@ -14,6 +14,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.facebook.react.bridge.JSIModulePackage;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -54,6 +55,7 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
   }
 
   /**
