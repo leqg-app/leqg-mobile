77a8e0ce0a5c37dff358f590be002795e7f631bc
Nicolas de Chevigné <nico2che@gmail.com>
2022-02-08 00:12:40 -0800

    Ignore some logs on dev mode

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ import { StoreProvider } from './store/context';
 import Routes from './Routes';
 import { theme } from './constants';
 
-// @gorhom/bottom-sheet@4.1.5
-LogBox.ignoreLogs([
-  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
-]);
-
 if (!__DEV__) {
   Sentry.init({
     dsn: 'https://247aa8fba4ca46688925bf9823ba239e@o1079194.ingest.sentry.io/6083816',
     tracesSampleRate: 1.0,
   });
+} else {
+  // @gorhom/bottom-sheet@4.1.5
+  LogBox.ignoreLogs([
+    "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+  ]);
 }
 
 const App = () => {
