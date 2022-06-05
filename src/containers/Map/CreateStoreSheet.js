031d5123c8c3f22519d7e0dba48088e3013359cd
Nicolas de Chevigné <nico2che@gmail.com>
2022-06-05 18:14:47 -0400

    Add store only when user is logged

--- src/containers/Map/CreateStoreSheet.js
+++ src/containers/Map/CreateStoreSheet.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useRef } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import { Title } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import ActionSheet from '../../components/ActionSheet';
 import ActionButtons from '../../components/ActionButtons';
+import { storeEditionState, userState } from '../../store/atoms';
 
 function getStateText(createStore) {
   if (!createStore) {
@@ -26,17 +28,51 @@ function getStateText(createStore) {
 const CreateStoreSheet = ({ createStore, onClose }) => {
   const navigation = useNavigation();
   const sheet = useRef();
+  const user = useRecoilValue(userState);
+  const setStoreEdition = useSetRecoilState(storeEditionState);
 
   const close = React.useCallback(() => {
-    sheet.current.close();
+    sheet.current?.close?.();
     onClose();
   });
 
+  const submit = () => {
+    if (!user) {
+      Alert.alert(
+        '',
+        'Vous devez être connecté pour ajouter un nouveau bar',
+        [
+          {
+            text: 'Annuler',
+            style: 'cancel',
+          },
+          {
+            text: 'Connexion',
+            onPress: () => {
+              navigation.navigate('AccountTab');
+              close();
+            },
+          },
+        ],
+        { cancelable: true },
+      );
+      return;
+    }
+    setStoreEdition(createStore);
+    navigation.navigate('EditStoreScreen', {
+      screen: 'EditStore',
+      params: { store: createStore },
+    });
+  };
+
   useEffect(() => {
+    if (!sheet.current) {
+      return;
+    }
     if (createStore) {
       sheet.current.snapToIndex(0);
     } else {
-      sheet.current?.close();
+      sheet.current.close();
     }
   }, [createStore]);
 
@@ -47,12 +83,7 @@ const CreateStoreSheet = ({ createStore, onClose }) => {
         <Text>{getStateText(createStore)}</Text>
         <ActionButtons
           onCancel={close}
-          onSubmit={() =>
-            navigation.navigate('EditStoreScreen', {
-              screen: 'EditStore',
-              params: { store: createStore },
-            })
-          }
+          onSubmit={submit}
           submitLabel="Ajouter"
           disabled={!createStore?.address}
         />
