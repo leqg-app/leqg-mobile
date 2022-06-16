752f0673c4b8502708222fb2fb5b79eeae0cd219
Nicolas de Chevigné <nico2che@gmail.com>
2022-06-16 17:56:40 +0300

    Fix restart crash on Android

--- android/app/src/main/java/com/leqg/MainActivity.java
+++ android/app/src/main/java/com/leqg/MainActivity.java
@@ -1,5 +1,6 @@
 package com.leqg;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.zoontek.rnbootsplash.RNBootSplash;
@@ -13,6 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "leqg";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   @Override
