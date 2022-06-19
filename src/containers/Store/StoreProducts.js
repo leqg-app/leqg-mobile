50ad23d04b759d67b114b069d1608c8603bef934
Nicolas de Chevigné <nico2che@gmail.com>
2022-06-20 00:53:04 +0300

    Improve product display

--- src/containers/Store/StoreProducts.js
+++ src/containers/Store/StoreProducts.js
@@ -7,65 +7,99 @@ import Price from '../../components/Price';
 import { sortByPrices } from '../../utils/price';
 import { productsState } from '../../store/atoms';
 
+// TODO: load types from API
 const types = {
   draft: 'Pression',
   bottle: 'Bouteille',
 };
 
+function groupByType(types, product) {
+  if (!types[product.type]) {
+    types[product.type] = {};
+  }
+  const id = product.productId || product.productName.trim();
+  if (!types[product.type][id]) {
+    types[product.type][id] = { ...product, prices: [] };
+  }
+  types[product.type][id].prices.push(product);
+  return types;
+}
+
+function sortByType(a) {
+  return a.type === 'draft' ? -1 : 1;
+}
+
 function StoreProducts(props) {
   const products = useRecoilValue(productsState);
 
   const hasHH = props.products.some(product => product.specialPrice);
-  // TODO: bold happy hour if we currently are
+
+  const productsByTypes = Array.from(props.products)
+    .sort(sortByType)
+    .sort(sortByPrices)
+    .reduce(groupByType, {});
+
   return (
     <View>
-      <View style={styles.row}>
+      <View style={[styles.row, styles.headRow]}>
         <Text style={styles.title}>Bières</Text>
-        <View style={styles.prices}>
-          <Text style={styles.pricesCell}>Prix</Text>
-          {hasHH && <Text style={styles.pricesCell}>HH.</Text>}
-        </View>
       </View>
-      {Array.from(props.products)
-        .sort(sortByPrices)
-        .map((product, i) => {
-          const { volume, price, specialPrice, productName, currencyCode } =
-            product;
-          const productDetail = products.find(
-            ({ id }) => id === product.productId,
-          );
-          return (
-            <View key={i} style={styles.row}>
-              <View style={{ flex: 1 }}>
-                <Text numberOfLines={1}>
-                  {productDetail?.name || productName || 'Bière'}
-                </Text>
-                <Caption>
-                  {types[product.type]}
-                  {volume && ` - ${volume}cl`}
-                </Caption>
-              </View>
+      {Object.keys(productsByTypes).map(type => (
+        <View key={type}>
+          <View style={styles.row}>
+            <Text style={styles.typeTitle}>{types[type] || 'Autre'}</Text>
+            {hasHH && (
               <View style={styles.prices}>
-                <Text style={styles.pricesCell}>
-                  {price ? (
-                    <Price amount={price} currency={currencyCode} />
-                  ) : (
-                    '-'
-                  )}
-                </Text>
-                {hasHH && (
-                  <Text style={styles.pricesCell}>
-                    {specialPrice ? (
-                      <Price amount={specialPrice} currency={currencyCode} />
-                    ) : (
-                      ' '
-                    )}
-                  </Text>
-                )}
+                <Text style={styles.pricesCell}>Prix</Text>
+                <Text style={styles.pricesCell}>HH.</Text>
               </View>
-            </View>
-          );
-        })}
+            )}
+          </View>
+          {Object.values(productsByTypes[type]).map((product, i) => {
+            const { productName, currencyCode } = product;
+            const productDetail = products.find(
+              ({ id }) => id === product.productId,
+            );
+            return (
+              <View key={i} style={styles.row}>
+                <View style={{ flex: 1 }}>
+                  <Text numberOfLines={1}>
+                    {productDetail?.name || productName || 'Bière'}
+                  </Text>
+                </View>
+                <View>
+                  {product.prices.map(({ price, volume, specialPrice }) => {
+                    return (
+                      <View style={styles.prices} key={volume}>
+                        <Caption>{`${volume}cl`}</Caption>
+                        <Text style={styles.pricesCell}>
+                          {price ? (
+                            <Price amount={price} currency={currencyCode} />
+                          ) : (
+                            '-'
+                          )}
+                        </Text>
+                        {hasHH && (
+                          <Text style={styles.pricesCell}>
+                            {specialPrice ? (
+                              <Price
+                                amount={specialPrice}
+                                currency={currencyCode}
+                              />
+                            ) : (
+                              ' '
+                            )}
+                          </Text>
+                        )}
+                      </View>
+                    );
+                  })}
+                </View>
+              </View>
+            );
+          })}
+        </View>
+      ))}
     </View>
   );
 }
@@ -79,9 +113,9 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   row: {
-    height: 55,
-    paddingTop: 3,
-    paddingLeft: 15,
+    minHeight: 45,
+    paddingVertical: 8,
+    marginLeft: 15,
     paddingRight: 10,
     display: 'flex',
     flexDirection: 'row',
@@ -90,16 +124,26 @@ const styles = StyleSheet.create({
     borderBottomColor: '#ddd',
     borderBottomWidth: StyleSheet.hairlineWidth,
   },
+  headRow: {
+    marginLeft: 0,
+    paddingLeft: 15,
+    height: 55,
+  },
+  typeTitle: {
+    fontWeight: 'bold',
+  },
   productName: {
     flex: 1,
   },
   prices: {
     display: 'flex',
     flexDirection: 'row',
+    alignItems: 'center',
   },
   pricesCell: {
     width: 50,
-    textAlign: 'center',
+    marginLeft: 10,
+    textAlign: 'left',
   },
 });
 
