1524c3f7f7a5f6a64e5dd9b748e57c8f8a39d29d
Nicolas de Chevigné <nico2che@gmail.com>
2022-11-22 00:27:22 +0100

    Upgrade dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        extensions: ['.tsx', '.ts', '.js', '.json'],
-      },
-    ],
-    'react-native-reanimated/plugin',
-  ],
-  env: {
-    test: {
-      plugins: ['react-native-paper/babel'],
-    },
-    production: {
-      plugins: ['react-native-paper/babel'],
-    },
-  },
+  plugins: ['react-native-paper/babel', 'react-native-reanimated/plugin'],
 };
