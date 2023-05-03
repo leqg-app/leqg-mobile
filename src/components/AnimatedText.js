894b9f6557293454b55b20ea68eb51ca919337f6
Nicolas de Chevigné <nico2che@gmail.com>
2023-05-03 22:55:21 +0200

    Fix theme on won reputation screen

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -1,12 +1,12 @@
 import React, { useRef, useEffect } from 'react';
 import { Animated, Easing, StyleSheet, TextInput } from 'react-native';
-
-import { theme } from '../constants';
+import { useTheme } from 'react-native-paper';
 
 const AnimateTextInput = Animated.createAnimatedComponent(TextInput);
 
 function AnimatedText({ initial, won }) {
   const inputRef = useRef();
+  const { colors } = useTheme();
   const animated = new Animated.Value(initial);
 
   useEffect(() => {
@@ -17,6 +17,7 @@ function AnimatedText({ initial, won }) {
         });
       }
     });
+
     Animated.timing(animated, {
       toValue: initial + won,
       duration: 2000,
@@ -31,14 +32,13 @@ function AnimatedText({ initial, won }) {
       underlineColorAndroid="transparent"
       editable={false}
       defaultValue={initial.toString()}
-      style={styles.text}
+      style={[styles.text, { color: colors.primary }]}
     />
   );
 }
 
 const styles = StyleSheet.create({
   text: {
-    color: theme.colors.primary,
     fontWeight: '900',
     fontSize: 20,
     textAlign: 'center',
