ab90d04e170dcf161e36325fa1636dd174c1c20b
Nicolas de Chevigné <nico2che@gmail.com>
2026-01-25 21:25:40 +0100

    Revert "Try true sheet"
    This reverts commit a3172d1da0fe635eb7fe74061c1d6b13f3dc8a05.

--- src/containers/Map/StoreSheet.js
+++ src/containers/Map/StoreSheet.js
@@ -1,37 +1,43 @@
-import React, { useEffect, useRef, Suspense } from 'react';
-import {
-  BackHandler,
-  Pressable,
-  ScrollView,
-  StyleSheet,
-  View,
-  Dimensions,
-} from 'react-native';
-import { IconButton, Text, useTheme } from 'react-native-paper';
-import { TrueSheet } from '@lodev09/react-native-true-sheet';
+import React, { useEffect, useState, useMemo, useRef, Suspense } from 'react';
+import { BackHandler, Pressable, StyleSheet, View } from 'react-native';
+import { IconButton, Portal, Text, Title, useTheme } from 'react-native-paper';
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
+import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet';
 import { useFocusEffect } from '@react-navigation/native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useAtom } from 'jotai';
 import { ErrorBoundary } from 'react-error-boundary';
 import StarRating from 'react-native-star-rating-widget';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import SchedulesPreview from '../Store/SchedulesPreview';
 import Store from '../Store/Store';
 import { sheetStoreState } from '../../store/atoms';
+import StoreSheetMenu from './StoreSheetMenu';
 import StoreRateCount from './StoreRateCount';
 
 const StoreSheet = () => {
   const { colors } = useTheme();
-  const insets = useSafeAreaInsets();
   const sheet = useRef(null);
+  const lastSheetStore = useRef(null);
   const [sheetStore, setSheetStore] = useAtom(sheetStoreState);
+  const [previewHeight, setPreviewHeight] = useState(0);
+  const [show, setShow] = useState(false);
+  const sheetPosition = useSharedValue(0);
+  const { top, bottom } = useSafeAreaInsets();
+
+  const topbarHeight = top + 50;
+  const sheetHeight = bottom + 170;
 
   useFocusEffect(() => {
     const event = BackHandler.addEventListener(
       'hardwareBackPress',
       function () {
         if (sheetStore) {
-          sheet.current.dismiss();
+          setSheetStore();
           return true;
         }
         return false;
@@ -42,74 +48,142 @@ const StoreSheet = () => {
 
   useEffect(() => {
     if (!sheet.current) {
+      setShow(false);
       return;
     }
     if (sheetStore) {
-      sheet.current.present(0);
+      if (!lastSheetStore.current) {
+        sheet.current.snapToIndex(0);
+      }
+      setTimeout(() => setShow(true), 300);
     } else {
-      sheet.current.dismiss();
+      sheet.current.close();
+      setShow(false);
     }
+    lastSheetStore.current = sheetStore;
   }, [sheetStore]);
 
+  const initialSnapPoints = useMemo(() => [sheetHeight, '100%'], []);
+
+  const animatedTopBar = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateY: interpolate(
+          sheetPosition.value,
+          [0.5, 1],
+          [-topbarHeight, 0],
+        ),
+      },
+    ],
+    opacity: sheetPosition.value,
+  }));
+  const topBarStyle = useMemo(() => animatedTopBar, [animatedTopBar]);
+
+  const getPreviewHeight = event =>
+    setPreviewHeight(event.nativeEvent.layout.height - 40);
+
+  const animatedContent = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateY: interpolate(
+          sheetPosition.value,
+          [0.5, 1],
+          [0, -previewHeight + (sheetStore?.rate ? 20 : -2)],
+        ),
+      },
+    ],
+    opacity: sheetPosition.value,
+  }));
+  const sheetStyle = { backgroundColor: colors.background, minHeight: '100%' };
+  const contentStyle = useMemo(
+    () => [sheetStyle, animatedContent],
+    [animatedContent],
+  );
+
   return (
-    <TrueSheet
-      ref={sheet}
-      detents={['0.4', '1']}
-      scrollable
-      onDidDismiss={() => setSheetStore()}
-      maxHeight={Dimensions.get('window').height - insets.top}
-      header={
-        <View style={styles.previewHeader}>
-          <Text variant="headlineLarge" style={styles.title}>
-            {sheetStore?.name}
-          </Text>
-          <View style={styles.closeButtonWrapper}>
-            <IconButton
-              icon="close"
-              size={20}
-              style={styles.closeButton}
-              onPress={() => setSheetStore()}
-            />
-          </View>
-        </View>
-      }>
-      <ScrollView nestedScrollEnabled>
-        <Pressable onPress={() => sheet.current.resize(1)}>
-          <View>
-            {sheetStore?.rate ? (
-              <View style={styles.rateRow}>
-                <Text style={styles.rate}>
-                  {sheetStore?.rate.toFixed(1).replace('.', ',')}
-                </Text>
-                <StarRating
-                  rating={sheetStore?.rate}
-                  starSize={15}
-                  starStyle={styles.stars}
-                  onChange={() => {}}
-                  color={colors.primary}
-                />
-                {sheetStore?.id && (
-                  <ErrorBoundary fallback={<></>}>
-                    <Suspense fallback={<></>}>
-                      <StoreRateCount id={sheetStore.id} />
-                    </Suspense>
-                  </ErrorBoundary>
-                )}
+    <Portal>
+      {show && (
+        <Animated.View
+          style={[
+            styles.topBarWrapper,
+            topBarStyle,
+            {
+              height: topbarHeight,
+              paddingTop: top,
+              backgroundColor: colors.background,
+            },
+          ]}>
+          <IconButton
+            size={30}
+            icon="chevron-down"
+            onPress={() => sheet.current.snapToIndex(0)}
+          />
+          {sheetStore?.id && (
+            <ErrorBoundary fallback={<></>}>
+              <Suspense fallback={<></>}>
+                <StoreSheetMenu id={sheetStore.id} />
+              </Suspense>
+            </ErrorBoundary>
+          )}
+        </Animated.View>
+      )}
+      <BottomSheet
+        ref={sheet}
+        index={-1}
+        snapPoints={initialSnapPoints}
+        animatedIndex={sheetPosition}
+        topInset={topbarHeight - 20}
+        handleStyle={{ backgroundColor: colors.background }}
+        backgroundStyle={{ backgroundColor: colors.background }}
+        enablePanDownToClose
+        onClose={() => sheetStore && setSheetStore()}>
+        <BottomSheetScrollView contentContainerStyle={sheetStyle}>
+          <Pressable
+            onPress={() => sheet.current.snapToIndex(1)}
+            onLayout={getPreviewHeight}>
+            <View style={styles.previewContainer}>
+              <Title numberOfLines={1} style={styles.title}>
+                {sheetStore?.name}
+              </Title>
+              {sheetStore?.rate ? (
+                <View style={styles.rateRow}>
+                  <Text style={styles.rate}>
+                    {sheetStore?.rate.toFixed(1).replace('.', ',')}
+                  </Text>
+                  <StarRating
+                    rating={sheetStore?.rate}
+                    starSize={15}
+                    starStyle={styles.stars}
+                    onChange={() => {}}
+                    color={colors.primary}
+                  />
+                  {sheetStore?.id && (
+                    <ErrorBoundary fallback={<></>}>
+                      <Suspense fallback={<></>}>
+                        <StoreRateCount id={sheetStore.id} />
+                      </Suspense>
+                    </ErrorBoundary>
+                  )}
+                </View>
+              ) : null}
+              <View style={styles.preview}>
+                <View style={styles.previewSchedules}>
+                  {sheetStore?.schedules && (
+                    <SchedulesPreview schedules={sheetStore.schedules} />
+                  )}
+                </View>
+                <Text numberOfLines={1}>{sheetStore?.address}</Text>
               </View>
-            ) : null}
-            <View style={styles.preview}>
-              <View style={styles.previewSchedules}>
-                {sheetStore?.schedules && (
-                  <SchedulesPreview schedules={sheetStore.schedules} />
-                )}
-              </View>
-              <Text numberOfLines={1}>{sheetStore?.address}</Text>
             </View>
-          </View>
-        </Pressable>
-        <Store sheetStore={sheetStore} />
-      </ScrollView>
-    </TrueSheet>
+          </Pressable>
+          {show && (
+            <Animated.View style={contentStyle}>
+              <Store sheetStore={sheetStore} />
+            </Animated.View>
+          )}
+        </BottomSheetScrollView>
+      </BottomSheet>
+    </Portal>
   );
 };
 
@@ -126,25 +200,18 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingHorizontal: 10,
   },
-  previewHeader: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 30,
-    marginBottom: 10,
+  previewContainer: {
+    minHeight: 93,
+    marginBottom: 20,
   },
   title: {
-    marginTop: 5,
-    marginLeft: 15,
     fontWeight: 'bold',
-    lineHeight: 28,
-    fontSize: 23,
-    flex: 1,
+    marginHorizontal: 15,
   },
   rateRow: {
     flexDirection: 'row',
-    marginBottom: 6,
     marginHorizontal: 15,
+    marginTop: 5,
   },
   rate: {
     color: 'grey',
@@ -156,24 +223,14 @@ const styles = StyleSheet.create({
   },
   preview: {
     marginHorizontal: 16,
-    marginBottom: 5,
+    marginBottom: 15,
   },
   previewSchedules: {
+    marginTop: 6,
     marginBottom: 8,
   },
   loading: {
     marginTop: 10,
-  },
-  closeButtonWrapper: {
-    alignItems: 'flex-end',
-    marginRight: 15,
-    minWidth: 60,
-  },
-  closeButton: {
-    backgroundColor: '#eee',
-    display: 'flex',
-    alignContent: 'center',
-    justifyContent: 'center',
   },
 });
 
